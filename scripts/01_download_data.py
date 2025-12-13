3a86bde57f5e17346fa2cda71b44f53aafc4f558
Apoorva Srivastava <apoorva.srivastav001@gmail.com>
2025-12-13 11:00:25 -0800

    Minor edit for resolving file paths

--- scripts/01_download_data.py
+++ scripts/01_download_data.py
@@ -7,6 +7,11 @@ import click
 import requests
 import zipfile
 import os
+import sys
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, ROOT)
+
 from src.create_directory import create_directory
 
 @click.command()
--- scripts/02_validate_data.py
+++ scripts/02_validate_data.py
@@ -9,6 +9,11 @@ import pandera.pandas as pa
 import logging
 import json
 import os
+import sys
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, ROOT)
+
 from src.validate_file_format import validate_file_format
 from src.validate_column_names import validate_column_names
 
