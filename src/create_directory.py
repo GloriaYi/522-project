1d353dc2cbec8e46c4f1be7994da2e54d73f7668
Apoorva Srivastava <apoorva.srivastav001@gmail.com>
2025-12-13 09:48:19 -0800

    Adding function to create directory in src folder

---
+++ src/create_directory.py
@@ -0,0 +1,22 @@
+# create_directory.py
+
+import os
+
+def create_directory(directory_path):
+    """
+    Create a directory if it doesn't exist.
+
+    Parameters
+    ----------
+    directory_path : str
+        Path to the directory to create.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> create_directory("data/raw")
+    """
+    os.makedirs(directory_path, exists_ok=True)
